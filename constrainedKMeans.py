2b9faa7861f3de2ebf3d3e202391ecce2abf8bc9
Giulio Fernando Lopes Paiva Denardi <Giulio_Denardi@intuit.com>
2017-08-15 05:22:22 -0300

    Testing and adding fixes. TODO: we need to update the __getInitialClusters to remove the points that are already in cls.values() or else it may result in a division by 0 error. TODO: Test with some arbitrary constraints.

--- constrainedKMeans.py
+++ constrainedKMeans.py
@@ -1,115 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import random as rand;
-import math
+import math;
 from sets import Set;
+import copy;
+import numpy as np;
 
 ## mlCons / dlCons structure: [(instance, instance), ... (instance, instance)]
 ## instance / point structure: Set(attr1, attr2...)
 class ConstrainedKMeans:
-	def __init__(self, clustersQty, convergeThreshold, distFunction):
-		self.clustersQty = clustersQty;
-		self.convergeThreshold = convergeThreshold;
-		self.distFunction = distFunction;
+    def __init__(self, clustersQty, convergeThreshold, distFunction):
+        self.clustersQty = clustersQty;
+        self.convergeThreshold = convergeThreshold;
+        self.distFunction = distFunction;
 
-	#This functiion trains with the dataset.
-	def clusterize(self, dataset, mlCons, dlCons):
-		print('clusterizing with ', self.clustersQty, " clusters...");
-		self.clusters = {k : rand.choice(dataset) for k in range(self.clustersQty)}
-		self.clusterPoints = {k : [] for k in self.clusters.keys()};
-		self.oldClusters = self.clusters; # TODO: Remove this line
-		
-		while (not self.__converged()):
-			self.clusterPoints = {k : [] for k in self.clusters.keys()}
-			self.__assignPoints(dataset, mlCons, dlCons);
-			self.oldClusters = self.clusters;
-			self.clusters = self.__updateClusters(self.clusterPoints);
+    #This functiion trains with the dataset.
+    def clusterize(self, dataset, mlCons, dlCons):
+        print('clusterizing with ', self.clustersQty, " clusters...");
+        self.clusters = self.__getInitialClusters(dataset.copy());
+        self.oldClusters = None;
+        
+        while (not self.__converged()):
+            self.clusterPoints = {k : [] for k in self.clusters.keys()}
+            self.__assignPoints(dataset, mlCons, dlCons);
+            self.oldClusters = copy.deepcopy(self.clusters);
+            self.__updateClusters();
 
-		return self.clusterPoints;
+        print('Cluster x Points: ', self.clusterPoints);
+        print('Clusters: ', self.clusters);
+        return self.clusterPoints;
 
-	#This function preprocess the dataset.
-	#It should put values in [0,1] and transform symbolic data into numerical data.
-	def __preprocessDataset(self, dataset):
-    		return None
+    #This function shall check if the function has stop converging (we should limit a threshold)
+    def __converged(self):
+        if (self.oldClusters != None):
+            for i in self.oldClusters.keys():
+                if (abs(np.std(self.oldClusters[i]) - np.std(self.clusters[i])) > self.convergeThreshold):
+                    print('CONVERGE MORE!')
+                    return False;
+        else:
+            return False;
 
-	#This function shall check if the function has stop converging (we should limit a threshold)
-	def __converged(self):
-		if (self.oldClusters != None):
-    			for i in self.oldClusters.items:
-					for attr in self.oldClusters[i].length:
-						if (self.oldClusters[i][attr] - self.clusters[i][attr] > self.convergeThreshold):
-							return False;
+        return True;
 
-		return True;
+    #This function shall assign the points to the clusters according to its distance from the clusters.
+    def __assignPoints(self, dataset, mlCons, dlCons):
+        for point in dataset:
+            ##TODO check if should insert the points with constraints first.
+            cluster = self.__findNearestCluster(point);
+            if (not self.__violateConstraints(point, cluster, mlCons, dlCons)):
+                self.clusterPoints[cluster].append(point);
 
-	#This function shall assign the points to the clusters according to its distance from the clusters.
-	def __assignPoints(self, dataset, mlCons, dlCons):
-		for point in dataset:
-			##TODO check if should insert the points with constraints first.
-			cluster = self.__findNearestCluster(point);
-			if (not self.__violateConstraints(point, cluster, mlCons, dlCons)):
-				self.clusterPoints[cluster].append(point);
+    def __findNearestCluster(self, point):
+        choosenCluster = None;
+        choosenDist = None;
+        for c in self.clusters.items():
+            if (choosenCluster == None):
+                choosenCluster = c[0];
+                choosenDist = self.distFunction.getDist(point, c[1]);
+            elif (self.distFunction.getDist(point, c[1]) < choosenDist):
+                choosenCluster = c[0];
+                choosenDist = self.distFunction.getDist(point, c[1]);
 
-	def __findNearestCluster(self, point):
-		choosenCluster = None;
-		choosenDist = None;
-		for c in self.clusters:
-			if (choosenCluster == None):
-				choosenCluster = c;
-				choosenDist = self.distFunction.getDist(point);
-			elif (self.distFunction.getDist(point) < choosenDist):
-				choosenCluster = c;
-				choosenDist = self.distFunction.getDist(point);
-
-		return choosenCluster;
+        return choosenCluster;
 
 
-	#This function shall move the clusters according to its points' positions.
-	def __updateClusters(self, clusterPoints):
-		for cp in self.clusterPoints:
-			for attr in self.clusters[cp]:
-				self.clusters[cp][attr] = sum(x[attr] for x in self.clusterPoints[cp])/len(self.clusterPoints[cp]);
+    #This function shall move the clusters according to its points' positions.
+    def __updateClusters(self):
+        for cp in self.clusterPoints.keys():
+            for attr in range(0, self.clusters[cp].size):
+                self.clusters[cp][attr] = sum(x[attr] for x in self.clusterPoints[cp])/len(self.clusterPoints[cp]);
 
+    #This function gets the initial clusters, avoiding to get the same cluster point at the same time.
+    ##TODO do this better.
+    def __getInitialClusters(self, dataset):
+        if (np.unique(dataset).size < self.clustersQty):
+            raise ValueError('O número de instâncias únicas do dataset deve ser maior ou igual o número de grupos.');
 
-	#This function is the article's violate-contraint function.
-	def __violateConstraints(self, point, cluster, mlCons, dlCons):
-		print('checking constraints...');
-		mustLink = [x for x in mlCons if (point == x or point[::-1] == x)];
+        cls = {};
+        for i in range(0, self.clustersQty):
+            candidate = rand.choice(range(0, len(dataset)));
 
-		if (mustLink.length > 0):
-			for ml in mustLink:
-				if (ml[0] == point):
-					if (ml[1] not in cluster):
-						return True;
-				else:
-					if (ml[0] not in cluster):
-						return True;
+            if (len(cls.values()) > 0):
+                print('TODO check if the value exists in the cls.values.')
 
-		dontLink = [x for x in dlCons if (point == x or point[::-1] == x)];
+            cls[i] = dataset[candidate];
 
-		if (dontLink.length > 0):
-			for dl in dontLink:
-				if (dl[0] == point):
-					if (ml[1] in cluster):
-						return True;
-				else:
-					if (ml[0] in cluster):
-						return True;
+        return cls;
 
-		return False;
+    #This function is the article's violate-contraint function.
+    def __violateConstraints(self, point, cluster, mlCons, dlCons):
+        mustLink = [x for x in mlCons if (point == x or point[::-1] == x)];
+
+        if (len(mustLink) > 0):
+            for ml in mustLink:
+                if (ml[0] == point):
+                    pairCluster = self.__findNearestCluster(ml[1]);
+                else:
+                    pairCluster = self.__findNearestCluster(ml[0]);
+                if (pairCluster != cluster):
+                    return True;
+
+        dontLink = [x for x in dlCons if (point == x or point[::-1] == x)];
+
+        if (len(dontLink) > 0):
+            for dl in dontLink:
+                if (dl[0] == point):
+                    pairCluster = self.__findNearestCluster(dl[1]);
+                else:
+                    pairCluster = self.__findNearestCluster(dl[0]);
+                if (pairCluster != cluster):
+                    return True;
+
+        return False;
 
 
 class DistanceMetrics:
 
-    def EuclidianDistance(self, X, Y):
-        tuples = zip(X, Y)
-        distance = 0
-        for x, y in tuples:
-            distance += (x ** 2 - y ** 2)
-        return math.sqrt(distance)
-
-    def SimpleMatchDistance(self, X, Y):
-        tuples = zip(X, Y)
-        distance = 0
-        for x, y in tuples:
-            if(x != y):
-                distance += 1
-        return distance
+    class EuclidianDistance:
+        def getDist(self, X, Y):
+            tuples = zip(X, Y)
+            distance = 0
+            for x, y in tuples:
+                distance += (x - y) ** 2
+            return math.sqrt(distance)
+        
+    class SimpleMatchDistance:
+        def getDist(self, X, Y):
+            tuples = zip(X, Y)
+            distance = 0
+            for x, y in tuples:
+                if(x != y):
+                    distance += 1
+            return distance
--- testCluster.py
+++ testCluster.py
@@ -4,17 +4,21 @@ from dataProcessor import Processor
 from constrainedKMeans import ConstrainedKMeans, DistanceMetrics
 
 # Dataset loading
+print('getting dataset...')
 dataset_raw = genfromtxt('./datasets/soybean-small.clean.data', delimiter=',', dtype=None) # numeric only dataset
+print('Got dataset!');
 
 # Data pre-processing
+print('Starting data processing...');
 processor = Processor(dataset_raw)
 processor.apply_scaling() # just in case the whole dataset is composed by numeric attributes
 dataset = processor.get_data()
+print('Data processed! dataset: ', dataset);
 
 # Algorithm execution
 k_clusters = 4 # number of classes from the original dataset
-converge_threshold = 1.0
-distance_metric = DistanceMetrics.EuclidianDistance # just in case the whole dataset is composed by numeric attributes
+converge_threshold = 0.02
+distance_metric = DistanceMetrics.EuclidianDistance() # just in case the whole dataset is composed by numeric attributes
 constrained_kmeans = ConstrainedKMeans(k_clusters, converge_threshold, distance_metric)
 
 must_link = []
